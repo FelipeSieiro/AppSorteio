6da13ea8633a15b6bc77e4c478784a728d3fa074
FelipeSieiro <felipedossantos0804@gmail.com>
2024-08-26 22:45:53 -0300

    feat random number

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [numero, setNumero] = useState(null);
+
+  const sortearNumero = () => {
+    const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+    setNumero(numeroAleatorio);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Sorteio de Números</Text>
+      
+      {/* Botão para sortear um número */}
+      <Button title="Sortear Número" onPress={sortearNumero} />
+      
+      {/* Exibindo o número sorteado */}
+      {numero !== null && <Text style={styles.result}>{numero}</Text>}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  result: {
+    fontSize: 32,
+    marginTop: 20,
   },
 });
+
+export default App;
